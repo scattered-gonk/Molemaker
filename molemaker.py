06fbbe5e05b1e4c19452c14ac755eaa039a8ffef
scattered-gonk <scattered_gonk@proton.me>
2025-10-08 13:40:51 -0500

    Enhanced more readability

--- molemaker.py
+++ molemaker.py
@@ -5,7 +5,7 @@ from mole import Mole
 parser = argparse.ArgumentParser()
 parser.add_argument("FILE_NAME")
 args = parser.parse_args()
-
+ 
 mole_args = [vars(args)[item] for item in vars(args).keys()]
 mole = Mole(mole_args)
 
